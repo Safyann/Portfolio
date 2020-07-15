26d28f715fdbbbc680cc5c97499b20c14e3ab6d1
Alena Safyannikova <alena_safyanniko@mail.ru>
2020-07-15 22:16:16 +0500

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,30 @@
-const webpack = require('webpack');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const webpack = require("webpack");
+const UglifyJSPlugin = require("uglifyjs-webpack-plugin");
 
 const config = {
-    entry: "./app/js/main.js",
+  entry: "./src/js/main.js",
 
-    module: {
-        loaders: [{
-            test: /.js?$/,
-            loader: 'babel-loader',
-            exclude: /node_modules/,
-            query: {
-                presets: ['es2015', 'env']
-            }
-        }]
-    },
+  module: {
+    loaders: [
+      {
+        test: /.js?$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+        query: {
+          presets: ["es2015", "env"],
+        },
+      },
+    ],
+  },
 
-    output: {
-        filename: 'main.min.js'
-    },
-    plugins: [
-        new UglifyJSPlugin({
-            sourceMap: true
-        })
-    ]
+  output: {
+    filename: "main.min.js",
+  },
+  plugins: [
+    new UglifyJSPlugin({
+      sourceMap: true,
+    }),
+  ],
 };
 
 module.exports = config;
